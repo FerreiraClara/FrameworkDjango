774eb7b96ca5318113b0c613deecd5ed6580cdb6
Clara Rocha Ferreira <115419127+FerreiraClara@users.noreply.github.com>
2023-09-21 13:43:39 -0300

    Primeiro Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def print_hi():
+    print(f'Hello World!!')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi()
+
+""" No TERMINAL:
+
+pip install Django  ->  para instalar o Django
+
+django-admin startproject pythonProject - Django
+
+python main.py runserver
+"""
